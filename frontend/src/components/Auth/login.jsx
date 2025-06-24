5a904dc1f39f4bb0ec2f056abf7a200bb293cede
Muhammad Shoaib <shoaib14702580@gmail.com>
2025-06-24 07:18:48 +0500

    update deploy urlh

--- frontend/src/components/Auth/login.jsx
+++ frontend/src/components/Auth/login.jsx
@@ -2,107 +2,107 @@
 
 
 
-// import React, { useContext, useState } from "react";
-// import { MdOutlineMailOutline } from "react-icons/md";
-// import { RiLock2Fill } from "react-icons/ri";
-// import { Link, useNavigate } from "react-router-dom";
-// import { FaRegUser } from "react-icons/fa";
-// import axios from "axios";
-// import toast from "react-hot-toast";
-// import { Context } from "../../main";
+import React, { useContext, useState } from "react";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLock2Fill } from "react-icons/ri";
+import { Link, useNavigate } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Context } from "../../main";
 
-// const Login = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [role, setRole] = useState("");
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
 
-//   const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
-//   const navigate = useNavigate(); // ✅ use navigate instead of <Navigate>
+  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
+  const navigate = useNavigate(); // ✅ use navigate instead of <Navigate>
 
-//   const handleLogin = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const { data } = await axios.post(
-//         "https://job-seeking-mern-app-8ujq.vercel.app/api/v1/user/login",
-//         { email, password, role },
-//         {
-//           headers: { "Content-Type": "application/json" },
-//           withCredentials: true,
-//         }
-//       );
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "https://job-seeking-mern-app-8ujq.vercel.app/api/v1/user/login",
+        { email, password, role },
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
 
-//       toast.success(data.message);
+      toast.success(data.message);
 
-//       // ✅ Update context properly
-//       setUser(data.user);
-//       setIsAuthorized(true);
+      // ✅ Update context properly
+      setUser(data.user);
+      setIsAuthorized(true);
 
-//       // ✅ Navigate to home after login
-//       navigate("/");
-//     } catch (error) {
-//       toast.error(error?.response?.data?.message || "Login failed");
-//     }
-//   };
+      // ✅ Navigate to home after login
+      navigate("/");
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Login failed");
+    }
+  };
 
-//   return (
-//     <>
-//       <section className="authPage">
-//         <div className="container">
-//           <div className="header">
-//             <img src="/JobZeelogo.png" alt="logo" />
-//             <h3>Login to your account</h3>
-//           </div>
-//           <form onSubmit={handleLogin}>
-//             <div className="inputTag">
-//               <label>Login As</label>
-//               <div>
-//                 <select value={role} onChange={(e) => setRole(e.target.value)} required>
-//                   <option value="">Select Role</option>
-//                   <option value="Employer">Employer</option>
-//                   <option value="Job Seeker">Job Seeker</option>
-//                 </select>
-//                 <FaRegUser />
-//               </div>
-//             </div>
-//             <div className="inputTag">
-//               <label>Email Address</label>
-//               <div>
-//                 <input
-//                   type="email"
-//                   placeholder="zk@gmail.com"
-//                   value={email}
-//                   onChange={(e) => setEmail(e.target.value)}
-//                   required
-//                 />
-//                 <MdOutlineMailOutline />
-//               </div>
-//             </div>
-//             <div className="inputTag">
-//               <label>Password</label>
-//               <div>
-//                 <input
-//                   type="password"
-//                   placeholder="Your Password"
-//                   value={password}
-//                   onChange={(e) => setPassword(e.target.value)}
-//                   required
-//                 />
-//                 <RiLock2Fill />
-//               </div>
-//             </div>
-//             <button type="submit">Login</button>
-//             <Link to={"/register"}>Register Now</Link>
-//           </form>
-//         </div>
-//         <div className="banner">
-//           <img src="/login.png" alt="login" />
-//         </div>
-//       </section>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <section className="authPage">
+        <div className="container">
+          <div className="header">
+            <img src="/JobZeelogo.png" alt="logo" />
+            <h3>Login to your account</h3>
+          </div>
+          <form onSubmit={handleLogin}>
+            <div className="inputTag">
+              <label>Login As</label>
+              <div>
+                <select value={role} onChange={(e) => setRole(e.target.value)} required>
+                  <option value="">Select Role</option>
+                  <option value="Employer">Employer</option>
+                  <option value="Job Seeker">Job Seeker</option>
+                </select>
+                <FaRegUser />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Email Address</label>
+              <div>
+                <input
+                  type="email"
+                  placeholder="zk@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <MdOutlineMailOutline />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Password</label>
+              <div>
+                <input
+                  type="password"
+                  placeholder="Your Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <RiLock2Fill />
+              </div>
+            </div>
+            <button type="submit">Login</button>
+            <Link to={"/register"}>Register Now</Link>
+          </form>
+        </div>
+        <div className="banner">
+          <img src="/login.png" alt="login" />
+        </div>
+      </section>
+    </>
+  );
+};
 
-// export default Login;
+export default Login;
 
 
 
@@ -143,105 +143,105 @@
 // my code
 
 
-import React, { useContext, useState } from "react";
-import { MdOutlineMailOutline } from "react-icons/md";
-import { RiLock2Fill } from "react-icons/ri";
-import { Link, Navigate } from "react-router-dom";
-import { FaRegUser } from "react-icons/fa";
-import axios from "axios";
-import toast from "react-hot-toast";
-import { Context } from "../../main";
+// import React, { useContext, useState } from "react";
+// import { MdOutlineMailOutline } from "react-icons/md";
+// import { RiLock2Fill } from "react-icons/ri";
+// import { Link, Navigate } from "react-router-dom";
+// import { FaRegUser } from "react-icons/fa";
+// import axios from "axios";
+// import toast from "react-hot-toast";
+// import { Context } from "../../main";
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [role, setRole] = useState("");
+// const Login = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [role, setRole] = useState("");
 
-  const { isAuthorized, setIsAuthorized } = useContext(Context);
+//   const { isAuthorized, setIsAuthorized } = useContext(Context);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const { data } = await axios.post(
-        "https://job-seeking-mern-app-8ujq.vercel.app/api/v1/user/login",
-        { email, password, role },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          withCredentials: true,
-        }
-      );
-      toast.success(data.message);
-      setEmail("");
-      setPassword("");
-      setRole("");
-      setIsAuthorized(true);
-    } catch (error) {
-      toast.error(error.response.data.message);
-    }
-  };
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const { data } = await axios.post(
+//         "https://job-seeking-mern-app-8ujq.vercel.app/api/v1/user/login",
+//         { email, password, role },
+//         {
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           withCredentials: true,
+//         }
+//       );
+//       toast.success(data.message);
+//       setEmail("");
+//       setPassword("");
+//       setRole("");
+//       setIsAuthorized(true);
+//     } catch (error) {
+//       toast.error(error.response.data.message);
+//     }
+//   };
 
-  if(isAuthorized){
-    return <Navigate to={'/'}/>
-  }
+//   if(isAuthorized){
+//     return <Navigate to={'/'}/>
+//   }
 
-  return (
-    <>
-      <section className="authPage">
-        <div className="container">
-          <div className="header">
-            <img src="/JobZeelogo.png" alt="logo" />
-            <h3>Login to your account</h3>
-          </div>
-          <form>
-            <div className="inputTag">
-              <label>Login As</label>
-              <div>
-                <select value={role} onChange={(e) => setRole(e.target.value)}>
-                  <option value="">Select Role</option>
-                  <option value="Employer">Employer</option>
-                  <option value="Job Seeker">Job Seeker</option>
-                </select>
-                <FaRegUser />
-              </div>
-            </div>
-            <div className="inputTag">
-              <label>Email Address</label>
-              <div>
-                <input
-                  type="email"
-                  placeholder="zk@gmail.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <MdOutlineMailOutline />
-              </div>
-            </div>
-            <div className="inputTag">
-              <label>Password</label>
-              <div>
-                <input
-                  type="password"
-                  placeholder="Your Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <RiLock2Fill />
-              </div>
-            </div>
-            <button type="submit" onClick={handleLogin}>
-              Login
-            </button>
-            <Link to={"/register"}>Register Now</Link>
-          </form>
-        </div>
-        <div className="banner">
-          <img src="/login.png" alt="login" />
-        </div>
-      </section>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <section className="authPage">
+//         <div className="container">
+//           <div className="header">
+//             <img src="/JobZeelogo.png" alt="logo" />
+//             <h3>Login to your account</h3>
+//           </div>
+//           <form>
+//             <div className="inputTag">
+//               <label>Login As</label>
+//               <div>
+//                 <select value={role} onChange={(e) => setRole(e.target.value)}>
+//                   <option value="">Select Role</option>
+//                   <option value="Employer">Employer</option>
+//                   <option value="Job Seeker">Job Seeker</option>
+//                 </select>
+//                 <FaRegUser />
+//               </div>
+//             </div>
+//             <div className="inputTag">
+//               <label>Email Address</label>
+//               <div>
+//                 <input
+//                   type="email"
+//                   placeholder="zk@gmail.com"
+//                   value={email}
+//                   onChange={(e) => setEmail(e.target.value)}
+//                 />
+//                 <MdOutlineMailOutline />
+//               </div>
+//             </div>
+//             <div className="inputTag">
+//               <label>Password</label>
+//               <div>
+//                 <input
+//                   type="password"
+//                   placeholder="Your Password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                 />
+//                 <RiLock2Fill />
+//               </div>
+//             </div>
+//             <button type="submit" onClick={handleLogin}>
+//               Login
+//             </button>
+//             <Link to={"/register"}>Register Now</Link>
+//           </form>
+//         </div>
+//         <div className="banner">
+//           <img src="/login.png" alt="login" />
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
 
-export default Login;
+// export default Login;
